7a146ca3c26ee77204616ee50c051e4c5b0d5407
100pecheneK <mistermihail23@gmail.com>
2020-12-13 14:08:57 +0500

    Рефакторинг имен и чуть-чуть магии

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from services.DishService import (
     get_dish_by_id
 )
 from services.CategoryService import get_all_category, create_category
-# TODO: отчистка консоли
-# TODO: предложение о создании категории или блюда если их нет в просмотре 
 import os
+
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 class NumberValidator(Validator):
     def validate(self, document):
@@ -25,22 +25,48 @@ class NumberValidator(Validator):
         except ValueError:
             raise ValidationError(
                 message='Пожалуйста введите целое число',
-                cursor_position=len(document.text))  # Move cursor to end
+                cursor_position=len(document.text))
+
+
+class RequiredValidator(Validator):
+    def validate(self, document):
+        if not len(document.text):
+            raise ValidationError(
+                message='Пожалуйста введите название',
+                cursor_position=len(document.text))
+
+
+def get_elem_choices_and_title_to_id(arr):
+    elem_choices = []
+    elem_title_to_id = {}
+    for elem in arr:
+        elem_choices.append(elem.title)
+        elem_title_to_id[elem.title] = elem.id
+    return elem_choices, elem_title_to_id
+
 
 def back():
     return
 
-def create_menu__create_dish_menu():
-    categories = get_all_category()
-    categories_choices = []
-    categories_title_to_id = {}
- 
-    for category in categories:
-        categories_choices.append(category.title)
-        categories_title_to_id[category.title] = category.id
 
-   
-    create_dish_menu_questions = [
+def return_menu(msg):
+    return_questions = [{'type': 'list', 'name': 'return_menu', 'message': '', 'choices': [
+        'Назад']}]
+
+    return_aswers = {
+        'Назад': back
+    }
+    print(msg)
+    answer = prompt(return_questions, style=CUSTOM_TERMINAL_STYLES)
+    return return_aswers[answer['return_menu']]()
+
+
+def create_dish_menu():
+    categories = get_all_category()
+    category_choices, category_title_to_id = get_elem_choices_and_title_to_id(
+        categories)
+
+    create_dish_questions = [
         {
             'type': 'input',
             'name': 'title',
@@ -50,7 +76,7 @@ def create_menu__create_dish_menu():
             'type': 'list',
             'name': 'category',
             'message': 'Выберите категорию: ',
-            'choices': categories_choices,
+            'choices': category_choices,
         },
         {
             'type': 'input',
@@ -84,11 +110,12 @@ def create_menu__create_dish_menu():
             'filter': lambda val: int(val)
         },
     ]
+
     clear_terminal()
-    answer = prompt(create_dish_menu_questions, style=CUSTOM_TERMINAL_STYLES)
+    answer = prompt(create_dish_questions, style=CUSTOM_TERMINAL_STYLES)
 
     selected_category = answer['category']
-    selected_category_id = categories_title_to_id[selected_category]
+    selected_category_id = category_title_to_id[selected_category]
 
     data = {
         "title": answer['title'],
@@ -101,87 +128,79 @@ def create_menu__create_dish_menu():
             "components": answer['components']
         }
     }
-    
-    create_dish(data)
-    
 
-def create_menu__create_category_menu():
-    category_questions = [{
-            'type': 'input',
-            'name': 'title',
-            'message': 'Название категории: '
+    create_dish(data)
+
+
+def create_category_menu():
+    create_category_questions = [{
+        'type': 'input',
+        'name': 'title',
+        'message': 'Название категории: ',
+        'validate': RequiredValidator
     }]
     clear_terminal()
-    answer = prompt(category_questions, style=CUSTOM_TERMINAL_STYLES)
+    answer = prompt(create_category_questions, style=CUSTOM_TERMINAL_STYLES)
     create_category(answer)
-    
 
-def main_menu__find():
+
+def find_menu():
     print('Меню поиска')
 
-def main_menu__view():
+
+def view_category_menu():
+
     categories = get_all_category()
     if not len(categories):
-        print('Создайте категорию')
-        answer = prompt([{'type':'list', 'name':'view','message':'','choices':['Назад']}], style=CUSTOM_TERMINAL_STYLES)
-        return back()
+        return return_menu('Создайте категорию')
 
-    categories_choices = []
-    categories_title_to_id = {}
- 
-    for category in categories:
-        categories_choices.append(category.title)
-        categories_title_to_id[category.title] = category.id
+    category_choices, category_title_to_id = get_elem_choices_and_title_to_id(
+        categories)
 
-    view_categories_questions = [{
-            'type': 'list',
-            'name': 'category',
-            'message': 'Выберите категорию: ',
-            'choices': categories_choices,
+    view_category_questions = [{
+        'type': 'list',
+        'name': 'category',
+        'message': 'Выберите категорию: ',
+        'choices': category_choices,
     }]
-    answer = prompt(view_categories_questions, style=CUSTOM_TERMINAL_STYLES)
+    answer = prompt(view_category_questions, style=CUSTOM_TERMINAL_STYLES)
     selected_category = answer['category']
-    selected_category_id = categories_title_to_id[selected_category]
+    selected_category_id = category_title_to_id[selected_category]
 
     dishes = get_dish_by_category_id(selected_category_id)
-
     if not len(dishes):
-        print('Создайте блюда')
-        answer = prompt([{'type':'list', 'name':'view','message':'','choices':['Назад']}], style=CUSTOM_TERMINAL_STYLES)
-        return back()
-    
-    dishes_choices = []
-    dishes_title_to_id = {}
- 
-    for dish in dishes:
-        dishes_choices.append(dish.title)
-        dishes_title_to_id[dish.title] = dish.id
+        return return_menu('Создайте блюда')
+
+    view_dish_choices, dish_title_to_id = get_elem_choices_and_title_to_id(
+        dishes)
 
     view_dish_questions = [{
-            'type': 'list',
-            'name': 'dish',
-            'message': 'Выберите блюдо: ',
-            'choices': dishes_choices,
+        'type': 'list',
+        'name': 'dish',
+        'message': 'Выберите блюдо: ',
+        'choices': view_dish_choices,
     }]
     answer = prompt(view_dish_questions, style=CUSTOM_TERMINAL_STYLES)
     selected_dish = answer['dish']
-    selected_dish_id = dishes_title_to_id[selected_dish]
+    selected_dish_id = dish_title_to_id[selected_dish]
 
     dish = get_dish_by_id(selected_dish_id)
     pprint(dish.serialize())
-    answer = prompt([{'type':'list', 'name':'view','message':'','choices':['Назад']}], style=CUSTOM_TERMINAL_STYLES)
+    answer = prompt([{'type': 'list', 'name': 'view', 'message': '', 'choices': [
+                    'Назад']}], style=CUSTOM_TERMINAL_STYLES)
     back()
 
 
-def main_menu__quit_from_app():
-    print('Чао бИбА')
+def quit_from_app():
+    print('Чао бИбА with ❤️')
     exit()
 
-def main_menu__create():
+
+def create_menu():
     create_menu_answers = {
         'Назад': back,
-        'Новое блюдо': create_menu__create_dish_menu,
-        'Новая категория': create_menu__create_category_menu,
+        'Новое блюдо': create_dish_menu,
+        'Новая категория': create_category_menu,
     }
     create_menu_questions = [{
         'type': 'list',
@@ -193,35 +212,40 @@ def main_menu__create():
     if len(categories):
         create_menu_questions[0]['choices'].append('Новое блюдо')
 
-    answer = prompt(create_menu_questions, style=CUSTOM_TERMINAL_STYLES)   
+    answer = prompt(create_menu_questions, style=CUSTOM_TERMINAL_STYLES)
 
     create_menu_answers[answer['create_menu']]()
 
-main_menu_answers = {
-    'Найти': main_menu__find,
-    'Создать': main_menu__create,
-    'Просмотреть': main_menu__view,
-    'Выйти': main_menu__quit_from_app,
-}
-main_menu_questions = [{
-    'type': 'list',
-    'name': 'main_menu',
-    'message': 'Добро пожаловать в приложение!',
-    'choices': ['Найти', 'Создать', 'Просмотреть', 'Выйти'],
-}]
+
+def main_menu():
+    main_menu_answers = {
+        'Найти': find_menu,
+        'Создать': create_menu,
+        'Просмотреть': view_category_menu,
+        'Выйти': quit_from_app,
+    }
+    main_menu_questions = [{
+        'type': 'list',
+        'name': 'main_menu',
+        'message': 'Добро пожаловать в приложение!',
+        'choices': ['Найти', 'Создать', 'Просмотреть', 'Выйти'],
+    }]
+    answer = prompt(main_menu_questions, style=CUSTOM_TERMINAL_STYLES)
+    clear_terminal()
+    main_menu_answers[answer['main_menu']]()
+    clear_terminal()
 
 
 def init_client_actions():
     clear_terminal()
     work = True
     while work:
-        answer = prompt(main_menu_questions, style=CUSTOM_TERMINAL_STYLES)
-        clear_terminal()
-        main_menu_answers[answer['main_menu']]()
-        clear_terminal()
+        main_menu()
+
 
 def main():
     init_database()
     init_client_actions()
+
 
 main()
